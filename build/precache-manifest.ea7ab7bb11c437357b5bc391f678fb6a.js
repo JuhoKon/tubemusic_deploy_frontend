35a74afa6a53175c3336b6f6046a875d8c8f7a83
JuhoKon <kontiainen97@hotmail.fi>
2020-06-24 00:21:32 +0300

    initial commit

---
+++ build/precache-manifest.ea7ab7bb11c437357b5bc391f678fb6a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cda0352bf9b4a08defeee6ac177f2169",
+    "url": "/index.html"
+  },
+  {
+    "revision": "afa83648a685fb46527f",
+    "url": "/static/css/2.1dd414b0.chunk.css"
+  },
+  {
+    "revision": "ebced5e3d9ab3601b328",
+    "url": "/static/css/main.f3a4eb4b.chunk.css"
+  },
+  {
+    "revision": "afa83648a685fb46527f",
+    "url": "/static/js/2.917a4de3.chunk.js"
+  },
+  {
+    "revision": "a254756a57783ee3e33713cc1c74f082",
+    "url": "/static/js/2.917a4de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebced5e3d9ab3601b328",
+    "url": "/static/js/main.f8d78834.chunk.js"
+  },
+  {
+    "revision": "98d3a0e0bb7675af14c1",
+    "url": "/static/js/runtime-main.b4ac407d.js"
+  },
+  {
+    "revision": "24109cc55c1d837aa91dd55caf64ac24",
+    "url": "/static/media/cloud.24109cc5.svg"
+  }
+]);

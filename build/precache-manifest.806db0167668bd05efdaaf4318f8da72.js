1010b8e14a1573a825972b50f3d023caad63706d
Juho Kontiainen <juho.kontiainen@visma.com>
2020-07-07 07:17:50 +0300

    init

---
+++ build/precache-manifest.806db0167668bd05efdaaf4318f8da72.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9f01e219052ce257da3dd0b49dd085c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e1864768de0ae2ee19e",
+    "url": "/static/css/2.1dd414b0.chunk.css"
+  },
+  {
+    "revision": "05fa23fe5591f270bb79",
+    "url": "/static/css/main.ff16ced3.chunk.css"
+  },
+  {
+    "revision": "6e1864768de0ae2ee19e",
+    "url": "/static/js/2.7337c8d2.chunk.js"
+  },
+  {
+    "revision": "a254756a57783ee3e33713cc1c74f082",
+    "url": "/static/js/2.7337c8d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05fa23fe5591f270bb79",
+    "url": "/static/js/main.0b6b68c4.chunk.js"
+  },
+  {
+    "revision": "98d3a0e0bb7675af14c1",
+    "url": "/static/js/runtime-main.b4ac407d.js"
+  },
+  {
+    "revision": "20e831da2ff9597b77bee5587c2a89c4",
+    "url": "/static/media/cloud.20e831da.svg"
+  }
+]);

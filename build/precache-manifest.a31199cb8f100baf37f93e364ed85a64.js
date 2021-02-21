599d8eea323b3e1915f64f33b386034137c9956b
JuhoKon <kontiainen97@hotmail.fi>
2021-02-21 16:21:42 +0200

    New build

---
+++ build/precache-manifest.a31199cb8f100baf37f93e364ed85a64.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "827049d5dc71f63383f7ab30640bda60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "93c7433990da5d836efd",
+    "url": "/static/css/2.5cd3b1aa.chunk.css"
+  },
+  {
+    "revision": "4bb176fe65b1fdc35c53",
+    "url": "/static/css/main.3744b646.chunk.css"
+  },
+  {
+    "revision": "93c7433990da5d836efd",
+    "url": "/static/js/2.b914f4ea.chunk.js"
+  },
+  {
+    "revision": "a254756a57783ee3e33713cc1c74f082",
+    "url": "/static/js/2.b914f4ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bb176fe65b1fdc35c53",
+    "url": "/static/js/main.ea24b9bc.chunk.js"
+  },
+  {
+    "revision": "98d3a0e0bb7675af14c1",
+    "url": "/static/js/runtime-main.b4ac407d.js"
+  },
+  {
+    "revision": "24109cc55c1d837aa91dd55caf64ac24",
+    "url": "/static/media/cloud.24109cc5.svg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.867925eb928e174f9b8468f74edb1988.js"
+  "/precache-manifest.a31199cb8f100baf37f93e364ed85a64.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
